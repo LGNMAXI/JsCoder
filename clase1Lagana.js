3d5c26f2889c138fe74ec7d7d4a99e3d6dd785f5
Maximiliano <laganamaximiliano@gmail.com>
2021-11-02 10:00:21 -0300

    Trabajos clase 1 y 2

---
+++ clase1Lagana.js
@@ -0,0 +1,8 @@
+
+
+let nombre = prompt('Ingresar nombre')
+let apellido = prompt('Ingresar apellido')
+let usuario = (nombre + ' ' + apellido)
+
+alert('Bienvenido ' + (nombre + ' ' + apellido))
+console.log('Bienvenido ' + usuario)
---
+++ clase2Lagana.js
@@ -0,0 +1,27 @@
+
+// Ejercicio Números.
+
+let numero = parseInt(prompt('Ingrese un valor numérico'))
+
+
+if (numero > 1000){
+        alert("El número ingresado es mayor a 1000")
+
+}
+        else if (numero >= 10 && numero <= 50) {
+        alert("El número está entre 10 y 50")
+}
+        else {
+                console.log("No ingreso un número que genere una alerta.")
+}
+
+
+// Ejercicio Hola.
+
+let hola = prompt("Ingrese la palabra Hola")
+
+if (hola === "Hola") {
+        alert("Mostrar Alerta Hola!")
+} else {
+                console.log ("No dice Hola")
+}
