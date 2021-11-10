0160458d9daeaf1e8a53ed0a799612a2f8c9ecb1
Maximiliano <laganamaximiliano@gmail.com>
2021-11-09 21:41:22 -0300

    Cargo el trabajo práctico Nro. 3

---
+++ clase3Lagana.js
@@ -0,0 +1,40 @@
+// EJERCICIO 1
+
+/*
+let numero = parseInt(prompt(`ingresar un número`))
+
+for (let suma = numero; suma <= 5; suma ++) {
+        console.log (suma)
+}
+*/
+
+
+// EJERCICIO 2
+/*
+let palabraEsc = false;
+for (let i = 1; i <= 10  ; i++) {
+        let ingresarPalabra = prompt(`ingresar palabra`);
+
+        if (ingresarPalabra === `ESC`){
+                palabraEsc = true;
+                break;
+        } else {
+                console.log (`la palabrà ${ingresarPalabra} es incorrecta, va ${i} veces`)
+        }
+}
+
+if(palabraEsc) {
+        alert (`ingreso la palabra ESC, ciclo finalizado`)
+} else {
+        alert (`Intentos Agotados`)
+}
+*/
+
+// EJERCICIO 3
+/*
+let pedirNumero = parseInt(prompt( `Ingrese un número`))
+
+for (let i = 1; i <= pedirNumero; i++ ) {
+        console.log (`Hola`)
+}
+*/
